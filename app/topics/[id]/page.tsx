88fdf6199e24830517b9ddba2ba0026552b62343
𝐒𝐮𝐣𝐚𝐧 𝐃𝐚𝐬 <sd765715@gmail.com>
2025-09-15 17:33:58 +0530

    update topics page

--- app/topics/[id]/page.tsx
+++ app/topics/[id]/page.tsx
@@ -129,7 +129,7 @@ export default function TopicPage({ params }: TopicPageProps) {
       onClick={() => setActiveTab("code")}
       className="flex-shrink-0"
     >
-       Snippets
+      ⟨/⟩ Snippets
     </InteractiveButton>
   </div>
 </GlassmorphismCard>
